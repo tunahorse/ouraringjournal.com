1d96db269b87b7a254e36c23de58621b5dff170f
Fabian Anguiano <fabiananguiano@gmail.com>
2019-07-22 22:58:28 -0500

    homepage work

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -40,8 +40,8 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+              '#1F363D 0.5rem 0px 0px, #1F363D -0.5rem 0px 0px',
+            backgroundColor: '#1F363D',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
@@ -53,8 +53,8 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+              '#1F363D 0.5rem 0px 0px, #1F363D -0.5rem 0px 0px',
+            backgroundColor: '#1F363D',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
