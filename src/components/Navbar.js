28b332f1b9894c073d688179b4e0748780825866
Fabian Anguiano <fabiananguiano@gmail.com>
2019-07-27 18:37:26 -0500

    FUCK CSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,18 +63,18 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
+              {/* <Link className="navbar-item" to="/products">
                 Products
-              </Link>
+              </Link> */}
               <Link className="navbar-item" to="/blog">
-                Blog
+                Journals
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
+              {/* <Link className="navbar-item" to="/contact/examples">
                 Form Examples
-              </Link>
+              </Link> */}
             </div>
             <div className="navbar-end has-text-centered">
               <a
