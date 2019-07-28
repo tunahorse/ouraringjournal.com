097c9b7e68fb8c645c51c07c345eaa7fd280f019
Fabian Anguiano <fabiananguiano@gmail.com>
2019-07-27 19:01:20 -0500

    I hate css

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -16,13 +16,12 @@ export default class BlogIndexPage extends React.Component {
           <h1
             className="has-text-weight-bold is-size-1"
             style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
+              backgroundColor: '#1F363D',
               color: 'white',
               padding: '1rem',
             }}
           >
-            Latest Stories
+            Latest Journals
           </h1>
         </div>
         <section className="section">
